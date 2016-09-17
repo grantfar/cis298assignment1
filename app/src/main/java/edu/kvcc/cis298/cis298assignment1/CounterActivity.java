5b838836f073f76da7d23aa729d6191eaa999c0a
Grant Farnsworth <grantfar@gmail.com>
2016-09-17 00:38:33 -0400

    Created UI and made it add and subtract with the two buttons, and display a toast if the absolute value of the number is greater than 20

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,14 +4,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
 
+    private Button mMinusButton;
+    private Button mAdditionButton;
+    private TextView mDisplayText;
+    private int counterInt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+        mMinusButton = (Button) findViewById(R.id.minusButton);
+        mAdditionButton = (Button) findViewById(R.id.plusButton);
+        mDisplayText = (TextView) findViewById(R.id.displayTextView);
+        counterInt = 0;
+        mDisplayText.setText("0");
+        mAdditionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt++;
+                mDisplayText.setText((Integer.toString( counterInt)));
+                if(counterInt > 20 || counterInt < -20)
+                {
+                    Toast.makeText(CounterActivity.this,Integer.toString( counterInt),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        mMinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt--;
+                mDisplayText.setText((Integer.toString( counterInt)));
+                if(counterInt > 20 || counterInt < -20)
+                {
+                    Toast.makeText(CounterActivity.this,Integer.toString( counterInt),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
